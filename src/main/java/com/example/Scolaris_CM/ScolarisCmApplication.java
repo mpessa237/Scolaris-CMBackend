d68b27aeaf66baad24cf037515c6ca55ee97a033
mpessa237 <hervempessa7@gmail.com>
2026-07-12 12:57:21 +0100

    Initial commit: Scolaris-Backend Spring Boot

---
+++ src/main/java/com/example/Scolaris_CM/ScolarisCmApplication.java
@@ -0,0 +1,13 @@
+package com.example.Scolaris_CM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScolarisCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScolarisCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Scolaris_CM/ScolarisCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Scolaris_CM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScolarisCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
