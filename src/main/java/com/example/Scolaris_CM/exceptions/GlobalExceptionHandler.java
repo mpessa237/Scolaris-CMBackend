7b9723eccc3f51f5b4643cb84fab4da987bd7658
YouyouGlody <Youyoustyve@gmail.com>
2026-07-16 07:43:20 +0100

    Modification de l'import AccesDeniedException dans GlobalExceptionHandler

--- src/main/java/com/example/Scolaris_CM/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/Scolaris_CM/exceptions/GlobalExceptionHandler.java
@@ -4,13 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
