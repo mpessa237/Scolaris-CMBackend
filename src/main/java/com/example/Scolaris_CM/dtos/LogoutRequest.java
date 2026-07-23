3bbaa5aacf900c1e77ab23381948e90001e78a33
YouyouGlody <Youyoustyve@gmail.com>
2026-07-23 13:46:56 +0100

    Implementation du logout avec revocation du refresh token

---
+++ src/main/java/com/example/Scolaris_CM/dtos/LogoutRequest.java
@@ -0,0 +1,12 @@
+package com.example.Scolaris_CM.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogoutRequest {
+    private String refreshToken;
+}
