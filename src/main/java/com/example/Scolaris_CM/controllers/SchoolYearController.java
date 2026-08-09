1cfbbe0dee369bed750f39cc2ad4d52dcac2493e
YouyouGlody <Youyoustyve@gmail.com>
2026-08-09 08:09:58 +0100

    modififcation de Y en Y au niveau de RequestMapping api/school-years

--- src/main/java/com/example/Scolaris_CM/controllers/SchoolYearController.java
+++ src/main/java/com/example/Scolaris_CM/controllers/SchoolYearController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/school-Years")
+@RequestMapping("/api/school-y+-ears")
 @PreAuthorize("hasRole('ADMIN')")
 public class SchoolYearController {
 
